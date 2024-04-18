8a833b254bcb892c665d9e4abeb48d31a6f0bd5f
Cocoa2219 <10edit.remove01@gmail.com>
2024-04-18 22:49:22 +0900

    School

--- GenshinLauncher/MainWindow.xaml.cs
+++ GenshinLauncher/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Drawing;
+﻿using System.Diagnostics;
+using System.Drawing;
 using System.Text;
 using System.Windows;
 using System.Windows.Controls;
@@ -75,8 +76,67 @@ namespace GenshinLauncher
 
             if (btn != null)
             {
-                btn.Background = null;
-                btn.BorderBrush = null;
+                btn.Background = Brushes.Transparent;
+                btn.BorderBrush = Brushes.Transparent;
+            }
+        }
+
+        private void BtnStart_MouseEnter(object sender, MouseEventArgs e)
+        {
+            var btn = sender as Button;
+            var bc = new BrushConverter();
+
+            if (btn != null)
+            {
+                btn.Background = bc.ConvertFrom("#FFFFDB60") as Brush;
+                btn.BorderBrush = bc.ConvertFrom("#FFFFDB60") as Brush;
+            }
+        }
+
+        private void BtnStart_MouseLeave(object sender, MouseEventArgs e)
+        {
+            var btn = sender as Button;
+            var bc = new BrushConverter();
+
+            if (btn != null)
+            {
+                btn.Background = bc.ConvertFrom("#FFFFC70A") as Brush;
+                btn.BorderBrush = bc.ConvertFrom("#FFFFC70A") as Brush;
+            }
+        }
+
+        private void BtnIssues_Click(object sender, RoutedEventArgs e)
+        {
+            var psi = new ProcessStartInfo
+            {
+                FileName = "https://www.youtube.com/watch?v=9ixmwNCbo1c",
+                UseShellExecute = true
+            };
+
+            Process.Start(psi);
+        }
+
+        private void BtnStart_Click(object sender, RoutedEventArgs e)
+        {
+            var psi = new ProcessStartInfo
+            {
+                FileName = "https://namu.wiki/w/붕괴: 스타레일",
+                UseShellExecute = true
+            };
+
+            Process.Start(psi);
+        }
+
+        private void BtnSettings_Click(object sender, RoutedEventArgs e)
+        {
+            switch (GridStartingSettings.Visibility)
+            {
+                case Visibility.Visible:
+                    GridStartingSettings.Visibility = Visibility.Hidden;
+                    break;
+                case Visibility.Hidden:
+                    GridStartingSettings.Visibility = Visibility.Visible;
+                    break;
             }
         }
     }
